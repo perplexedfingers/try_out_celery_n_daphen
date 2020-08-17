e86226574f1a143f5160b21bb2bd9650be3eb0ad
Ray Zhu <ray.zhu1@hp.com>
2020-08-17 10:01:38 +0000

    Add tasks

---
+++ try_out/celery_n_daphen/tasks.py
@@ -0,0 +1,13 @@
+import time
+
+from ..try_out import celery_app
+
+
+def yo(self, retval, task_id, args, kwargs):
+    print(retval)
+
+
+@celery_app.task(on_success=yo)
+def long_running_operation():
+    time.sleep(5)
+    return 'done'
