83a572aa0dadadd7470ea3de22f1349784e48202
Ray Zhu <ray.zhu1@hp.com>
2020-08-17 10:22:57 +0000

    Update INSTALLED_APPS so that celery can work

--- try_out/try_out/settings.py
+++ try_out/try_out/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'celery_n_daphen.apps.CeleryNDaphenConfig',
 ]
 
 MIDDLEWARE = [
